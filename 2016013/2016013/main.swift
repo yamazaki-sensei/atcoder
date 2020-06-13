7f530f4d15112b3942d044e206e3462ec373233d
hira <ohno@toreta.in>
2020-06-13 21:58:03 +0900

    B

--- 2016013/2016013/main.swift
+++ 2016013/2016013/main.swift
@@ -5,3 +5,34 @@ class A {
     }
 }
 
+class B {
+
+    static func solve() {
+        let AV = readLine()!.split(separator: " ")
+        let A = Int64(AV[0])!
+        let V = Int64(AV[1])!
+        let BW = readLine()!.split(separator: " ")
+        let B = Int64(BW[0])!
+        let W = Int64(BW[1])!
+        let T = Int64(readLine()!)!
+
+        let diff = abs(B - A)
+        let diffPerSec = V - W
+        if A == B {
+            print("YES")
+            return
+        }
+        if diffPerSec <= 0  {
+            print("NO")
+            return
+        }
+
+        if diff <= diffPerSec * T {
+            print("YES")
+        } else {
+            print("NO")
+        }
+    }
+}
+
+B.solve()
